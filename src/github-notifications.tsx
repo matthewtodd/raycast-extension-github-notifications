68396b452fe2e80ba3e0f2d88dfaa5d54da6e79c
Matthew Todd <mt@bridge.xyz>
2026-07-24 10:40:41 -0400

    Revert "But don't leave a blank space"
    This reverts commit 3f71ce196b3d5c7faea23cd85c116f95418059a0.

--- src/github-notifications.tsx
+++ src/github-notifications.tsx
@@ -30,7 +30,9 @@ export default function Command() {
   // still need to return it (not null) while we're loading for Raycast to keep
   // the process alive.
   if (data.length === 0) {
-    return (isLoading) ? (<MenuBarExtra isLoading={isLoading} />) : null;
+    return (
+      <MenuBarExtra isLoading={isLoading} />
+    );
   }
 
   return (
